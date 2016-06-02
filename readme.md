baa3d004d59f6791fa128fc45c3792214a0fbea5
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-06-02 15:13:32 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Practice thai with thai trainer
+
+
---
+++ thai_trainer.rb
@@ -0,0 +1,27 @@
+require 'io/console'
+
+def print_letters
+   return "กขฃคฅฆงจฉชซ"
+end
+
+def start
+  letters_arr = print_letters.split("")
+  rand_letter_index = rand(letters_arr.size)
+  puts letters_arr[ rand_letter_index ]
+  input = STDIN.getch
+  while( input != "q") do
+    if(input != letters_arr[rand_letter_index])
+      	
+        puts "incorrect"
+        puts letters_arr[rand_letter_index]
+    else
+        rand_letter_index = rand(letters_arr.size)
+       puts letters_arr[rand_letter_index]
+    end
+    input = STDIN.getch
+  end
+end
+
+print_letters
+
+start
