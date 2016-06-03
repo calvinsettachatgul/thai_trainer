3f0df389a14c0392b310093a8527e53823af4043
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-06-02 18:21:26 -0700

    add consonants 1234

--- thai_trainer.rb
+++ thai_trainer.rb
@@ -1,16 +1,16 @@
 require 'io/console'
-first_consonants = "กขฃคฅฆงจฉชซ"
-second_consonants = ""
-third_consonants = ""
-fourth_consonants = ""
+@first_consonants = "กขฃคฅฆงจฉชซ"
+@second_consonants = "ฌญฎฏฐฑฒณดตถ"
+@third_consonants = "ทธนบปผฝพฟภม"
+@fourth_consonants = "ยรลวศษสหฬอฮ"
 
 
-def print_letters
-   return "กขฃคฅฆงจฉชซ"
+def print_letters(letter_set)
+   return letter_set
 end
 
 def start
-  letters_arr = print_letters.split("")
+  letters_arr = print_letters(@first_consonants).split("")
   rand_letter_index = rand(letters_arr.size)
   puts letters_arr[ rand_letter_index ]
   input = STDIN.getch
@@ -29,6 +29,6 @@ def start
   end
 end
 
-print_letters
+puts print_letters(@first_consonants)
 
 start
