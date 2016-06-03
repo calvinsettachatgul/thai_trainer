3b96268d731cd3d8d27c8f9fbb63085fc70c5dad
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-06-03 09:55:50 -0700

    trying to incorporate thai vowels

---
+++ vowels_test.rb
@@ -0,0 +1,11 @@
+require 'io/console'
+# input = STDIN.getch
+@first_consonants = "กขฃคฅฆงจฉชซ"
+@second_consonants = "ฌญฎฏฐฑฒณดตถ"
+@third_consonants = "ทธนบปผฝพฟภม"
+@fourth_consonants = "ยรลวศษสหฬอฮ"
+@first_vowels = "ะาเแ ิ ี ุ ู"
+@tones = " ่ ้ ๊ ๋ ็ ํ ์"
+
+p @first_vowels.split(" ")
+p @tones.split(" ")
