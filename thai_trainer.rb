329a3e146cf6745e54dbb2ebd659b75493553a92
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-06-03 19:24:34 -0700

    add streak count

--- thai_trainer.rb
+++ thai_trainer.rb
@@ -11,6 +11,7 @@ def print_letters(letter_set)
 end
 
 def start
+  streak_count = 0
   letters_arr = print_letters(@first_consonants).split("")
   rand_letter_index = rand(letters_arr.size)
   puts letters_arr[ rand_letter_index ]
@@ -21,8 +22,9 @@ def start
         puts "incorrect"
         puts letters_arr[rand_letter_index]
     else
+        streak_count += 1
 	puts input 
-	puts "correct"
+	puts "correct streak:#{streak_count}"
         rand_letter_index = rand(letters_arr.size)
         puts letters_arr[rand_letter_index]
     end
