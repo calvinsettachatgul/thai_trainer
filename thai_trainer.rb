d1035eb3a2752fa03f8eff075d65eae1a111efa9
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-06-10 22:21:41 -0700

    advance letters

--- thai_trainer.rb
+++ thai_trainer.rb
@@ -1,4 +1,5 @@
 require 'io/console'
+@streak_count = 0
 @first_consonants = "กขฃคฅฆงจฉชซ"
 @second_consonants = "ฌญฎฏฐฑฒณดตถ"
 @third_consonants = "ทธนบปผฝพฟภม"
@@ -11,28 +12,34 @@ def print_letters(letter_set)
    return letter_set
 end
 
-def advance_letter_set(streak)
+def advance_letter_set
 
-   return @all_letters[ streak % 4 ]
-   
+   return @all_letters[ ++level ]
+
 end
 
+
+
+
 def start
-  streak_count = 0
-  letters_arr = print_letters(@first_consonants).split("")
+  level = 0
+  letters_arr = print_letters(@all_letters[level]).split("")
   rand_letter_index = rand(letters_arr.size)
   puts letters_arr[ rand_letter_index ]
   input = STDIN.getch
   while( input != "q") do
     if(input != letters_arr[rand_letter_index])
-	streak_count = 0
-        puts input     	
+	      @streak_count = 0
+        puts input
         puts "incorrect"
         puts letters_arr[rand_letter_index]
     else
-        streak_count += 1
-	puts input 
-	puts "correct streak:#{streak_count}"
+        @streak_count += 1
+          if ( @streak_count > 19 )
+            letters_arr = print_letters(advance_letter_set).split("")
+          end
+	      puts input
+	      puts "correct streak:#{streak_count}"
         rand_letter_index = rand(letters_arr.size)
         puts letters_arr[rand_letter_index]
     end
